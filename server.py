013afaabdbfada519745781050503a184ffa3140
eh158 <eh158@duke.edu>
2018-11-18 12:45:47 -0500

    Added functions, removed test cases

--- server.py
+++ server.py
@@ -6,6 +6,70 @@ app = Flask(__name__)
 patientlib = {}
 
 
+def send_tachy_email(email):
+    print(email)
+
+
+def is_tachycardic(heart_rate, age):
+    tachy = [159, 166, 182, 179, 186, 169, 151, 137, 133, 130, 119, 100]
+    new_age = age * 365
+    if new_age <= 2:
+        if heart_rate >= tachy[0]:
+            return True
+        else:
+            return False
+    elif new_age <= 6:
+        if heart_rate >= tachy[1]:
+            return True
+        else:
+            return False
+    elif new_age <= 21:
+        if heart_rate >= tachy[2]:
+            return True
+        else:
+            return False
+    elif new_age <= 62:
+        if heart_rate >= tachy[3]:
+            return True
+        else:
+            return False
+    elif new_age <= 153:
+        if heart_rate >= tachy[4]:
+            return True
+        else:
+            return False
+    elif new_age <= 730:
+        if heart_rate >= tachy[5]:
+            return True
+        else:
+            return False
+    elif new_age <= 1460:
+        if heart_rate >= tachy[6]:
+            return True
+        else:
+            return False
+    elif new_age <= 2555:
+        if heart_rate >= tachy[7]:
+            return True
+        else:
+            return False
+    elif new_age <= 4015:
+        if heart_rate >= tachy[8]:
+            return True
+        else:
+            return False
+    elif new_age <= 5475:
+        if heart_rate >= tachy[9]:
+            return True
+        else:
+            return False
+    else:
+        if heart_rate >= tachy[10]:
+            return True
+        else:
+            return False
+
+
 @app.route("/api/new_patient/", methods=["POST"])
 def new_patient():
     r = request.get_json()
@@ -16,7 +80,7 @@ def new_patient():
         patientlib[patient] = {}
         patientlib[patient]['attending_email'] = r['attending_email']
         patientlib[patient]['user_age'] = r['user_age']
-        patientlib[patient]['heart_rate'] =[]
+        patientlib[patient]['heart_rate'] = []
         return jsonify('Initialized patient')
 
 
@@ -30,6 +94,52 @@ def heart_rate():
         return jsonify('Added HR')
     else:
         return jsonify('Patient not Initialized')
+
+
+@app.route("/api/status/<patient_id>", methods=["GET"])
+def heart_rate(patient_id):
+    HR_data = patientlib[patient_id]['heart_rate']
+    HR_data_cut = [i[0] for i in HR_data]
+    check = is_tachycardic(HR_data_cut)
+    if (check):
+        send_tachy_email(attending_email)
+
+
+@app.route("/api/status/<patient_id>", methods=["GET"])
+def get_status(patient_id):
+    HR_data = patientlib[patient_id]['heart_rate']
+    HR_data_cut = [i[0] for i in HR_data]
+    check = is_tachycardic(HR_data_cut)
+    if (check):
+        return jsonify('Tachycardic')
+    else:
+        return jsonify('Not Tachycardic')
+
+
+@app.route("/api/heart_rate/<patient_id>", methods=["GET"])
+def get_heart_rate(patient_id):
+    HR_data = patientlib[patient_id]['heart_rate']
+    HR_data_cut = [i[0] for i in HR_data]
+    return jsonify(HR_data_cut)
+
+
+@app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
+def get_heart_rate_avg(patient_id):
+    HR_data = patientlib[patient_id]['heart_rate']
+    HR_data_cut = [i[0] for i in HR_data]
+    avg_HR = sum(HR_data_cut) / len(HR_data_cut)
+    return jsonify(avg_HR)
+
+
+@app.route("/api/heart_rate/internal_average/", methods=["POST"])
+def internal_average():
+    r = request.get_json()
+    patient = r['patient_id']
+    target_time = r['heart_rate_average_since']
+    HR_data = patientlib[patient]['heart_rate']
+    HR_data_cut = [i[0] for i in HR_data if i[1] > target_time]
+    avg_HR = sum(HR_data_cut) / len(HR_data_cut)
+    return jsonify(avg_HR)
 
 
 if __name__ == "__main__":
--- test_server.py
+++ test_server.py
@@ -1,37 +1,3 @@
 import pytest
 import requests
 from server import *
-
-
-@pytest.mark.parametrize("patients, responses", [
-    ([[0, "hi@duke.edu", 10]], ["added"]),
-    ([[0, "hi@duke.edu", 10], [0, "hi@duke.edu", 10]],
-     ["Initialized patient", "Already initialized"])
-])
-def test_new_patient(patients, responses):
-    results = []
-    for i in patients:
-        r = requests.post("http://127.0.0.1:5000/api/new_patient/",
-                          json={"patient_id": i[0],
-                                "attending_email": i[1],
-                                "user_age": i[2]})
-        results.append(r.json())
-    for j in range(len(results)):
-        assert results[j] == responses[j]
-
-
-@pytest.mark.parametrize("patients, responses", [
-    ([[0, "hi@duke.edu", 10]], ["added"]),
-    ([[0, "hi@duke.edu", 10], [0, "hi@duke.edu", 10]],
-     ["Initialized patient", "Already initialized"])
-])
-def test_new_patient(patients, responses):
-    results = []
-    for i in patients:
-        r = requests.post("http://127.0.0.1:5000/api/new_patient/",
-                          json={"patient_id": i[0],
-                                "attending_email": i[1],
-                                "user_age": i[2]})
-        results.append(r.json())
-    for j in range(len(results)):
-        assert results[j] == responses[j]
