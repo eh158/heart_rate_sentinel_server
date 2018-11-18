683947fd8872d8e9c541460116488cc5658b6acf
eh158 <eh158@duke.edu>
2018-11-18 11:38:50 -0500

    Added HR function

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -15,7 +16,20 @@ def new_patient():
         patientlib[patient] = {}
         patientlib[patient]['attending_email'] = r['attending_email']
         patientlib[patient]['user_age'] = r['user_age']
+        patientlib[patient]['heart_rate'] =[]
         return jsonify('Initialized patient')
+
+
+@app.route("/api/heart_rate/", methods=["POST"])
+def heart_rate():
+    r = request.get_json()
+    if r['patient_id'] in patientlib.keys():
+        patient = r['patient_id']
+        hrdata = [r['heart_rate'], datetime.datetime.now()]
+        patientlib[patient]['heart_rate'].append(hrdata)
+        return jsonify('Added HR')
+    else:
+        return jsonify('Patient not Initialized')
 
 
 if __name__ == "__main__":
